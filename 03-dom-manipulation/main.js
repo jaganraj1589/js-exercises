f5db0d423fe94063e0800fdcd1256e57e8173222
jaganraj g <jaganraj1589@gmail.com>
2019-05-06 09:35:06 +0530

    Update main.js
    new changes

--- 03-dom-manipulation/main.js
+++ 03-dom-manipulation/main.js
@@ -1,33 +1,35 @@
 const columns = document.querySelectorAll('.classifiedColumn')
-const blocks = document.querySelectorAll('.pocket ul li')
+const pocketBlock = document.getElementById('pocket')
 const pockets = document.querySelector('.pocket ul')
 
-blocks.forEach(function(e){
-    e.addEventListener('click',function(){        
-        if(e.nodeName === 'LI'){
-            let letter = e.innerHTML.charAt(0)
-            if (letter === 'A'){
-                columns[0].appendChild(e)
-            }
-            if (letter === 'B'){
-                columns[1].appendChild(e)
-            }
-            if (letter === 'C'){
-                columns[2].appendChild(e)
-            }
-            for(let i=0; i < columns.length; i++){
-                let list = columns[i];   
-                Array.from(list.querySelectorAll("li:not(:first-child)"))
-                .sort((a, b) =>{
-                    if (a.innerHTML > b.innerHTML) return 1
-                    else return -1
-                })
-                .forEach(li => list.appendChild(li));
-            }            
+
+const pocketClick = (e) =>{
+    let letter = e.target.innerHTML.charAt(0)
+    if(e.target.nodeName === 'LI'){
+        if (letter === 'A'){
+            columns[0].appendChild(e.target)
         }
+        if (letter === 'B'){
+            columns[1].appendChild(e.target)
+        }
+        if (letter === 'C'){
+            columns[2].appendChild(e.target)
+        }
+        for(let i=0; i < columns.length; i++){
+            let list = columns[i];
+            Array.from(list.querySelectorAll("li:not(:first-child)"))
+            .sort((a, b) =>{
+                if (a.innerHTML > b.innerHTML) return 1
+                else return -1
+            })
+            .forEach(li => list.appendChild(li));
+        }            
+
         if(pockets.children.length === 0){
             pocket.parentNode.removeChild(pocket)
         }
-    })
-})
+    }            
+        
+}
 
+pocketBlock.addEventListener('click', pocketClick);
