833550cdc9b93eb371e5ff423a692d6518cdf4fe
jaganraj g <jaganraj1589@gmail.com>
2019-06-26 12:48:17 +0530

    details added
    details added

--- 06-api-handling/main.js
+++ 06-api-handling/main.js
@@ -1,5 +1,4 @@
 const baseUrl = 'https://jsonplaceholder.typicode.com/posts';
-const detailUrl = 'https://jsonplaceholder.typicode.com/users/{user_id}';
 
 
 const list = document.getElementById('list')
@@ -12,9 +11,10 @@ fetch(baseUrl)
   .then(response => {
     return response.json()
   })
+  
   .then(data => {
     
-    // console.log(data)
+    console.log(data)
     
     data.forEach(e => {
         let li = createDocNode('li'); 
@@ -27,28 +27,55 @@ fetch(baseUrl)
         h2.innerHTML = e.title
         p.innerHTML = e.body
         btn.innerHTML = "View Details"
-        btn.setAttribute('data-id',`${e.id}`)
+        btn.setAttribute('data-id',`${e.userId}`)
         btn.setAttribute('data-target','modal1')
-        btn.setAttribute('class', 'btn-large waves-effect waves-light orange')
-        
+        btn.setAttribute('class', 'btn-small waves-effect waves-light red accent-4 modal-trigger')
+        btn.addEventListener('click', btnClick);
     });
-    const getDetails = document.querySelectorAll('.btn-large')
-    getDetails.forEach(e =>{
-      
-      e.addEventListener(click, function(i){
-        i.preventDefault();
-        console.log(e)
-      })
-    })
   })
+
+ 
   .catch(err => {
     
     // console.log(err)
   })
+  const pop = document.getElementById('popData')
+  const createPopNode = (e) => {
+      let elem =  document.createElement(e);
+      return pop.appendChild(elem)
+  }
 
+ function btnClick (){
+  popData.innerHTML= ''
+   let id = this.getAttribute('data-id')
+   fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+      .then(dresponse => {
+        return dresponse.json()
+      })
+      .then(detaildata => {
+        let title = createPopNode('h3')
+        let name = createPopNode('p')
+        let userName = createPopNode('p')
+        let email = createPopNode('p')
+        let company = createPopNode('p')
+        let address = createPopNode('p')
+        let phone = createPopNode('p')
+        let website = createPopNode('p')
 
+        title.innerHTML = 'Details'
+        name.innerHTML = `Name: <span>${detaildata.name}</span>`
+        userName.innerHTML = `Username: ${detaildata.username}`
+        email.innerHTML = `Email: <span>${detaildata.email}</span>`
+        company.innerHTML = `Company: <span>${detaildata.company.name}</span>`
+        address.innerHTML = `Address: ${detaildata.address.city}, ${detaildata.address.suite}, ${detaildata.address.zipcode}. `
+        phone.innerHTML = `Phone: <span>${detaildata.phone}</span>`
+        website.innerHTML = `Website: ${detaildata.website}`
+        console.log(detaildata)
+      })
+ }
 
+ 
   document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems, options);
+    var instances = M.Modal.init(elems);   
   });
